de9cee2fdf905f1f80ff6105c010698313817199
Tamer Zraiq <G00425053@atu.ie>
2024-10-01 10:56:48 +0100

    added hello and name

--- src/main/java/ie/atu/week3cicd/Week3.java
+++ src/main/java/ie/atu/week3cicd/Week3.java
@@ -1,15 +1,24 @@
 package ie.atu.week3cicd;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/person")
 public class Week3 {
+    @GetMapping("/hello")
+    public String message(){
+        return "Hello!";
+    }
     @GetMapping("/message")
     public String getMessage(){
         return "Some Message";
+    }
+    @GetMapping("/hello/{name}")
+    public String myName(@PathVariable String name){
+        return "Your Name is " + name;
     }
     @GetMapping("/details")
     public String getDetails(){
